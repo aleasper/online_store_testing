2486da2581b84cdc97c0b9520bc660e09bf19d36
aleasper <alexidest@gmail.com>
2019-07-25 16:40:23 +0300

    Add correction of test login/registration

--- test_main_page.py
+++ test_main_page.py
@@ -5,9 +5,11 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link) # initialization Page Object of main page
     page.open()
+    page.should_be_login_link()
     page.go_to_login_page()
-    new_page = LoginPage(page.browser, page.url)
-    new_page.should_be_login_url()
-    new_page.should_be_login_form()
-    new_page.should_be_register_form()
-    #page.should_be_login_link()
+
+    new_page = LoginPage(page.browser, browser.current_url)
+    new_page.should_be_login_page()
+    #new_page.should_be_login_url()
+    #new_page.should_be_login_form()
+    #new_page.should_be_register_form()

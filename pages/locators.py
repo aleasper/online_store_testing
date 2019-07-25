e05623774aa945cd78c047fec7216f6989d6a844
aleasper <alexidest@gmail.com>
2019-07-25 19:30:33 +0300

    Add test for product page

--- pages/locators.py
+++ pages/locators.py
@@ -12,3 +12,10 @@ class LoginPageLocators(object):
     REG_PASSWORD = (By.ID, "id_registration-password1")
     REG_REPEAT_PAS = (By.ID, "id_registration-password2")
     REG_BTN = (By.NAME, "registration_submit")
+
+class ProductPageLocators(object):
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".alertinner p strong")
+    BASKET_STRONG_NAMES = (By.CSS_SELECTOR, ".alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    BASKET_ADD_BTN = (By.CLASS_NAME, "btn-add-to-basket")
---
+++ test_product_page.py
@@ -0,0 +1,11 @@
+from .pages.product_page import ProductPage
+
+def test_guest_can_add_product_to_cart(browser):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019."
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_to_basket_btn()
+    page.add_item_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_item_in_basket()

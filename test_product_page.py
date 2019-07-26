83b60ae501184711994509082e74be81b5b43511
aleasper <alexidest@gmail.com>
2019-07-26 11:55:21 +0300

    Add 'go to login page from any page' ability

--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,23 @@ from .pages.product_page import ProductPage
 import pytest
 
 '''
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+'''
+
+'''
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    page.go_to_login_page()
+'''
+
+'''
 def test_guest_can_add_product_to_cart(browser):
     #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
